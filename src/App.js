c828aae1c27ec5c36344852849691e8959935b65
Teng-Ju Yang <tyang28@jhu.edu>
2024-07-25 18:38:37 -0500

    make login.css file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './cow.jpg';
-import './App.css';
+import logo from './cow.jpg'
+import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import Login from './components/login'
--- src/components/login.js
+++ src/components/login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import './login.css'
 
 const Login = (props) => {
   const [email, setEmail] = useState('')
