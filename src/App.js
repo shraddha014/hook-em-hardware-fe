72d81b20e85e2af6f0b39dbcc3800f83bcd43abb
shraddha014 <ms.shraddha14@gmail.com>
2024-07-29 21:54:30 -0500

    feat: added hardware checkin checkout ui (#21)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './cow.jpg'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { useEffect, useState } from 'react'
+import Hardware from './components/Hardware';
 import SelectExistingProject from './selectExistingProject'
 import Register from './Register.js';
 import Login from './components/login'
@@ -27,6 +28,7 @@ function App() {
       <div className="App-body">
         <BrowserRouter>
           <Routes>
+            <Route path="/hardware" element={<Hardware/>} />
             <Route path="/project-list" element={<SelectExistingProject/>} />
             <Route path="/register" element={<Register/>} />
             <Route path="/login" element={<Login/>} />
