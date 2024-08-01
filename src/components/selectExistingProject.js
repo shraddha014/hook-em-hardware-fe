af8d07bd02b0949868322657ae11f746239e7f5e
gowrivas <122569647+gowrivas@users.noreply.github.com>
2024-08-01 14:33:37 -0500

    Added css styling

--- src/components/selectExistingProject.js
+++ src/components/selectExistingProject.js
@@ -1,33 +1,34 @@
 
 import React from "react";
-
+import "./selectExistingProject.css";
 const SelectExistingProject = () => {
+  //made a more dynamic array, i hope this is what you were talking aboutt
+  const projects = [
+    { ProjectID: 1, Description: "Project 1 Description", url: "project1.html" },
+    { ProjectID: 2, Description: "Project 2 Description", url: "project2.html" },
+  ];
+
   const navigateToProject = (projectUrl) => {
     window.location.href = projectUrl;
   };
 
   return (
-    
     <header>
       Your Current Projects
-    
-    <div className="button-container">
-      <button style={{marginRight: 3 + 'em'} }
-        className="project-button" 
-        onClick={() => navigateToProject('project1.html')}
-      >
-        Project 1
-      </button>
-
-      <br>
-      </br>
-      <button style={{marginRight: 3 + 'em'}}
-        className="project-button" 
-        onClick={() => navigateToProject('project2.html')}
-      >
-        Project 2
-      </button>
-    </div>
+      <div className="button-container">
+        {projects.map((project) => (
+          <div key={project.ProjectID}>
+            <button
+              style={{ marginRight: '3em' }}
+              className="project-button"
+              onClick={() => navigateToProject(project.url)}
+            >
+              {`Project ${project.ProjectID}`}
+            </button>
+            <p>{project.Description}</p>
+          </div>
+        ))}
+      </div>
     </header>
   );
 };
