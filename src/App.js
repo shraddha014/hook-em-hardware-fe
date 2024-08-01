945febdc840be211e2df636a566a6d1902233c80
Shraddha <ms.shraddha14@gmail.com>
2024-07-31 20:42:04 -0500

    resolved conflict

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import React from 'react';
-import logo from './cow.jpg';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import logo from './cow.jpg'
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Hardware from './components/Hardware';
-import SelectExistingProject from './components/SelectExistingProject';
-import Register from './components/Register';
-import Login from './components/Login';
-import HomePage from './components/HomePage';
+import SelectExistingProject from './components/selectExistingProject.js'
+import Register from './components/Register.js';
+import Login from './components/login'
+import HomePage from './components/HomePage.js';
 import CreateNewProject from './components/CreateNewProject';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Hook'Em Hardware</h2>
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h2>
+          Hook'Em Hardware
+        </h2>
       </header>
       <div className="App-body">
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/hardware" element={<Hardware />} />
-            <Route path="/project-list" element={<SelectExistingProject />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
+            <Route path='/' element={<HomePage />} />
+            <Route path="/hardware" element={<Hardware/>} />
+            <Route path="/project-list" element={<SelectExistingProject/>} />
+            <Route path="/register" element={<Register/>} />
+            <Route path="/login" element={<Login/>} />
             <Route path="/create-new-project" element={<CreateNewProject />} />
           </Routes>
         </BrowserRouter>
